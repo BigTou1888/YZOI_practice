816ba417f5e0d318af16539416be75df30e9cbe5
MarbleYang <Shangda.Y@gmail.com>
2016-10-19 21:56:57 -0700

    add 1001 and 1008 python solution

---
+++ python_solution/language_learning/1008.py
@@ -0,0 +1,10 @@
+x = input().split()
+in_data = int(x[0])
+out_data = 0
+loop_cnt = 3
+while loop_cnt>0:
+    out_data = out_data * 10
+    out_data = out_data + in_data % 10
+    in_data = in_data // 10
+    loop_cnt = loop_cnt-1
+print (out_data)
