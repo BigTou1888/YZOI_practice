e24134dbd32fbc735278a3c617b1ad1dadeb93e8
Marbl <Shangda.Y@gmail.com>
2017-01-25 22:04:59 -0800

    pass version

--- python_solution/language_learning/1094.py
+++ python_solution/language_learning/1094.py
@@ -1,30 +1,24 @@
 import math
 
 v = input().split()
-a = float(v[0])
-b = float(v[1])
-c = float(v[2])
+p = float(v[0])
+w = float(v[1])
+l = float(v[2])
+ 
+r = 1
+offset = 0.02
+if l <= 300:
+    offset = 0
     
-if a == 0:
-    if b == 0:
-        if c == 0:
-            print("Many Roots")
-        else:
-            print("No Roots")
-    else:
-        x = 0-c/b
-        print ("%.2f" % x)
-else:
-    s = b*b - 4 *a *c
-    if s == 0:
-        x = 0-b/2/a
-        print ("%.2f" % x)
-    elif s < 0:
-        print ("No Roots")
-    else:
-        x1 = (0 - b - math.sqrt(s))/2/a
-        x2 = (0 - b + math.sqrt(s))/2/a
-        if x1 < x2:
-            print("%.2f %.2f" % (x1, x2))
-        else:
-            print("%.2f %.2f" % (x2, x1))
+if w <2:
+    r = 0.98
+elif (w< 5) & (w >=2):
+    r = 0.95
+elif (w>=5):
+    r = 0.92
+r = r + offset
+
+total = p * l * r
+
+
+print (round(total))
---
+++ python_solution/language_learning/1738.py
@@ -0,0 +1,11 @@
+import math
+
+v = input().split()
+s = int(v[0])
+e = int(v[1])
+
+ 
+if s>e:
+    print (s-e)
+else:
+    print(e-s)
